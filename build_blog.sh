9276dad6b1b55287a044fc61ea0a8383f29f8db1
gongshw <gongshw1992@gmail.com>
2013-12-03 21:21:01 +0800

    Latest build.

--- build_blog.sh
+++ build_blog.sh
@@ -1,9 +1,9 @@
 #!/bin/sh
 cd ~/raw.gongshw.github.io; 
+jekyll build || error_exit "$LINENO: jekyll build failed";
 git add .;
 git commit -am 'Latest build.';
 git push;
-jekyll build;
 cp -r _site/ ../gongshw.github.io/;
 cd ../gongshw.github.io/;
 git add .;
