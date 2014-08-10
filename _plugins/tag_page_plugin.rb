1facf3b3a387c6b8eeb6fac8ae52e90ce10962ea
gongshw <gongshw1992@gmail.com>
2014-08-10 16:34:51 +0800

    Latest build.

--- _plugins/tag_page_plugin.rb
+++ _plugins/tag_page_plugin.rb
@@ -8,8 +8,8 @@ module Jekyll
       self.process(@name)
       self.read_yaml(File.join(base, '_layouts'), 'tag_index.html')
       self.data['tag'] = tag
-      tag_title_prefix = site.config['tag_title_prefix'] || 'Posts Tagged &ldquo;'
-      tag_title_suffix = site.config['tag_title_suffix'] || '&rdquo;'
+      tag_title_prefix = site.config['tag_title_prefix'] || '有&ldquo;'
+      tag_title_suffix = site.config['tag_title_suffix'] || '&rdquo;标签的文章'
       self.data['title'] = "#{tag_title_prefix}#{tag}#{tag_title_suffix}"
     end
   end
