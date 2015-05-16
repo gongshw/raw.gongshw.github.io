acfec60ab7bbe26f70eae82cfbbcec56d9b7dd41
Shiwei Gong <gongshw1992@gmail.com>
2015-05-16 13:24:17 +0800

    Latest build: 本地化: 使用百度统计和多说

--- build_blog.sh
+++ build_blog.sh
@@ -2,11 +2,11 @@
 cd ~/raw.gongshw.github.io; 
 jekyll build || error_exit "$LINENO: jekyll build failed";
 git add .;
-git commit -am 'Latest build.';
+git commit -am "Latest build: ${1}";
 git push;
 cp -r _site/ ../gongshw.github.io/;
 cd ../gongshw.github.io/;
 git add .;
-git commit -am 'Latest build: ${1}';
+git commit -am "Latest build: ${1}";
 git push;
 
