e043c62962efb4354ffa65b35b539dc46132dd2e
Shiwei Gong <gongshw1992@gmail.com>
2015-08-07 02:11:24 -0400

    add swiftype search text field

--- js/main.js
+++ js/main.js
@@ -1,10 +1,29 @@
-$(document).ready(function () {
-	var str = location.href.toLowerCase();
-	$(".nav li a").each(function () {
-		var link = this.href.toLowerCase();
-		if (str == link) {
-			$("li.active").removeClass("active");
-			$(this).parent().addClass("active");
-		}
-	});
+$(document).ready(function() {
+    var str = location.href.toLowerCase();
+    $(".nav li a").each(function() {
+        var link = this.href.toLowerCase();
+        if (str == link) {
+            $("li.active").removeClass("active");
+            $(this).parent().addClass("active");
+        }
+    });
+
+    $search = $('#search_text');
+    $search.focus(function() {
+        $search.animate({
+            width: "190px",
+            cursor: "auto",
+            padding: "7px 11px 7px 28px"
+        }, function(argument) {
+            $search.click().val($search.val());
+        })
+    });
+
+    $search.blur(function() {
+        $search.animate({
+            width: "0px",
+            cursor: "pointer",
+            padding: "7px 11px 7px 20px"
+        })
+    });
 });
