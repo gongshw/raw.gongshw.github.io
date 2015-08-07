8b96ddd2b53fb2ad9f2a85f434c5ff7850e3a31d
Shiwei Gong <gongshw1992@gmail.com>
2015-08-06 23:04:33 -0400

    commit msg is required when deploying the blog

--- deploy_blog.sh
+++ deploy_blog.sh
@@ -1,13 +1,17 @@
 #!/bin/sh
+if [ $# -eq 0 ]; then
+    echo "git need a commit message!"
+    exit 1
+fi
 cd ~/raw.gongshw.github.io; 
 jekyll build || error_exit "$LINENO: jekyll build failed";
 git add .;
-git commit -am "Latest build: ${1}";
+git commit -am "${1}";
 git push;
 rm -rf ../gongshw.github.io/*;
 cp -r _site/* ../gongshw.github.io/;
 cd ../gongshw.github.io/;
 git add .;
-git commit -am "Latest build: ${1}";
+git commit -am "${1}";
 git push;
 
