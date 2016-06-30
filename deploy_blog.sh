144e187fa78edabca2edb13d186be906c309f941
Shiwei Gong <gongshw1992@gmail.com>
2016-06-30 08:32:12 -0400

    new deploy script

--- deploy_blog.sh
+++ deploy_blog.sh
@@ -3,15 +3,17 @@ if [ $# -eq 0 ]; then
     echo "git need a commit message!"
     exit 1
 fi
-cd ~/raw.gongshw.github.io; 
+RAW_PATH="~/raw.gongshw.github.io"
+SITE_PATH="~/gongshw.github.io"
+cd $RAW_PATH; 
 jekyll _2.5.3_ build || error_exit "$LINENO: jekyll build failed";
 git add .;
 git commit -am "${1}";
 git push;
-cd ../gongshw.github.io/;
+cd $SITE_PATH;
 git pull
-rm -rf ./*;
-cp -r ../raw.gongshw.github.io/_site/* .;
+rm -rf $SITE_PATH/*;
+cp -r $RAW_PATH/_site/* .;
 git add .;
 git commit -am "[auto sync] ${1}";
 git push;
