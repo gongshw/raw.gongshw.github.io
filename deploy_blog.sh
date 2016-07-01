870b767e402733869ab15d39a70eb5ae67ee7b92
Shiwei Gong <gongshw1992@gmail.com>
2016-07-01 08:26:24 -0400

    code syntax highlight enhancement

--- deploy_blog.sh
+++ deploy_blog.sh
@@ -3,8 +3,8 @@ if [ $# -eq 0 ]; then
     echo "git need a commit message!"
     exit 1
 fi
-RAW_PATH="~/raw.gongshw.github.io"
-SITE_PATH="~/gongshw.github.io"
+RAW_PATH="$HOME/raw.gongshw.github.io"
+SITE_PATH="$HOME/gongshw.github.io"
 cd $RAW_PATH; 
 jekyll _2.5.3_ build || error_exit "$LINENO: jekyll build failed";
 git add .;
