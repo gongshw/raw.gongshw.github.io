6323c828b564cffe59fc5e2b1e85613921f826b8
Shiwei Gong <gongshw1992@gmail.com>
2016-07-03 22:05:34 -0400

    .

--- deploy_blog.sh
+++ deploy_blog.sh
@@ -3,10 +3,13 @@ if [ $# -eq 0 ]; then
     echo "git need a commit message!"
     exit 1
 fi
+
 RAW_PATH="$HOME/raw.gongshw.github.io"
 SITE_PATH="$HOME/gongshw.github.io"
+JEKYLL_VERSION="2.5.3"
+
 cd $RAW_PATH; 
-jekyll _2.5.3_ build || error_exit "$LINENO: jekyll build failed";
+jekyll _${JEKYLL_VERSION}_ build || error_exit "$LINENO: jekyll build failed";
 git add .;
 git commit -am "${1}";
 git push;
